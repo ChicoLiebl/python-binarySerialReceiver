7ac965b6edb9711351cd6c1d5163bb68e374fae0
chicoliebl <chicolliebl@gmail.com>
2021-10-11 18:28:04 -0300

    added necessary PyPi files

--- Example/test-receiver.py
+++ Example/test-receiver.py
@@ -4,16 +4,24 @@ import time
 from binarySerialReceiver import binaryReceiver
 
 def main():
+  """ Defines the serial port """
+  """ Would be something like /de/ttyUSB0 or COM5 on linux and windows respectively. """
   port = './virtual-tty-recv'
+
+  """ Defines the arrays length """
   len = 100
   a = np.array([0] * len)
   b = np.array([0] * len)
   c = np.array([0] * len)
+
+  """ Initializes the receiver object """
   receiver = binaryReceiver([a, b, c], 'fff', port, 115200, packetHeader=b'\xFD\xFE')
   
   try:
+    """ Start the receiving thread """
     receiver.start()
   except Exception:
+    """ Exits if fails to fin the serial port """
     exit()
 
   try:
@@ -21,6 +29,8 @@ def main():
       print(a)
       time.sleep(0.5)
   except KeyboardInterrupt:
+    """ Recommend to call stop when KeyboardInterrupt happens 
+        so the background thread wonk keep active """
     receiver.stop()
 
 if __name__ == '__main__':
--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # Serial Receiver
 
-Simple serial receiver module for receiving binary data  
+Simple module for receiving binary data from a serial port in an asynchronous form
+
+## Description
+
+This modules lets you specify the format of the binary data and the header to be received from a serial port. The module creates a dedicated thread to read de serial port and feed the numpy arrays passed as buffers in a circular forma, always keeping the same length.
+
+## Example
+
+In the example folder there is a script that creates a virtual serial port (works only on linux) allowing you to run test-receiver.py which will print the received data buffer.
