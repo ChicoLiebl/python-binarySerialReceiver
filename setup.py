375f0d4638c8a99842297327f1205e067dfee9ad
chicoliebl <chicolliebl@gmail.com>
2021-10-11 19:41:56 -0300

    changed readme format

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from distutils.core import setup
 
 here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
